256b76a976dc1d129a27e790620bda3d5c7c6020
Hououin Kyouma <inwazzin@gmail.com>
2022-04-10 23:24:22 +0200

    thread id initialization

--- src/domain/thread.hpp
+++ src/domain/thread.hpp
@@ -4,8 +4,8 @@
 namespace thread {
   inline thread_local i32 ID = 0;
 
-  inline fn initialize() {
-    #pragma omp parallel default(none)
+  inline fn initialize(i32 max_cores = omp_get_max_threads()) {
+    #pragma omp parallel num_threads(max_cores) default(none)
     {
       ID = omp_get_thread_num();
     }
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,8 @@
 #include "imports.hpp"
-#include "utils/console.hpp"
 #include "domain/thread.hpp"
 
 fn main() -> i32 {
   thread::initialize();
-
-  #pragma omp parallel default(none)
-  {
-    console::log("Hello from thread %d", thread::ID);
-    console::event("Hello from thread %d", thread::ID);
-    console::info("Hello from thread %d", thread::ID);
-    console::error("Hello from thread %d", thread::ID);
-  }
 
   return 0;
 }
