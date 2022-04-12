63ac192d571061a1ef04f6bfde108042cf6d3c47
Hououin Kyouma <inwazzin@gmail.com>
2022-04-12 14:51:40 +0200

    candidate creation stub

--- src/domain/candidate.hpp
+++ src/domain/candidate.hpp
@@ -1,6 +1,30 @@
 #pragma once
 #include "../imports.hpp"
-#include "../utils/console.hpp"
+#include "instance.hpp"
+
+fn create_schedule(const Instance &instance, const vector<usize> &order) {
+  var schedule = vector<vector<usize>>(instance.M, vector<usize>(3 * instance.N, 0));
+
+  vector<usize> machine_state(instance.M, 0);
+  vector<usize> machine_time(instance.M, 0);
+  vector<usize> job_state(instance.N, 0);
+  vector<usize> job_time(instance.N, 0);
+
+  for (const auto &job: order) {
+    var job_step = job_state[job] << 1;
+    var machine = instance.Jobs[job_step][job];
+    ++job_state[job];
+    var start = std::max(machine_time[machine], job_time[job]);
+    var end = start + instance.Jobs[job][job_step + 1];
+    machine_time[machine] = end;
+    job_time[job] = end;
+
+    schedule[machine][machine_state[machine]++] = job;
+    schedule[machine][machine_state[machine]++] = start;
+    schedule[machine][machine_state[machine]++] = end;
+  }
+  return schedule;
+}
 
 class Candidate {
 public:
@@ -12,3 +36,18 @@ public:
   vector<usize> Order;
   vector<vector<usize>> Schedule;
 };
+
+fn find_makespan(vector<vector<usize>> schedule) {
+  usize makespan = 0;
+  for (auto machine: schedule) {
+    if (machine.back() < makespan) makespan = machine.back();
+  }
+  return makespan;
+}
+
+fn create_candidate(Instance instance, vector<usize> order) -> Candidate {
+  let schedule = create_schedule(instance, order);
+  let makespan = find_makespan(schedule);
+
+  return {makespan, order, schedule};
+}
